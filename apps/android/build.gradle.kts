4c408d4b54151431d576fe345c96eb41ead43529
Eliezer Graber <me@eygraber.com>
2024-01-04 02:56:05 -0500

    Make a Hello World Activity for Android

---
+++ apps/android/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+  kotlin("android")
+  id("com.android.application")
+  id("com.eygraber.conventions-kotlin-library")
+  id("com.eygraber.conventions-detekt")
+  id("com.eygraber.conventions-compose-jetbrains")
+}
+
+android {
+  namespace = "app.immich.kmp"
+
+  compileSdk = libs.versions.android.sdk.compile.get().toInt()
+
+  defaultConfig {
+    applicationId = "app.immich.kmp"
+    targetSdk = libs.versions.android.sdk.target.get().toInt()
+    minSdk = libs.versions.android.sdk.min.get().toInt()
+
+    versionCode = 1
+    versionName = "0.0.1"
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    multiDexEnabled = true
+  }
+
+  buildTypes {
+    named("release") {
+      isMinifyEnabled = true
+      isShrinkResources = true
+      proguardFiles.clear()
+      proguardFiles += project.file("proguard-rules.pro")
+    }
+
+    named("debug") {
+      applicationIdSuffix = ".debug"
+
+      versionNameSuffix = "-DEBUG"
+
+      isMinifyEnabled = false
+    }
+  }
+
+  compileOptions {
+    isCoreLibraryDesugaringEnabled = true
+  }
+
+  packaging {
+    resources.pickFirsts += "META-INF/*"
+  }
+
+  dependencies {
+    coreLibraryDesugaring(libs.android.desugar)
+
+    implementation(compose.material3)
+
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.activityCompose)
+    implementation(libs.androidx.appCompat)
+  }
+}
