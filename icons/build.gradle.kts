c940002edf838c727b3e81adba14c7dc8f76dba1
Eliezer Graber <me@eygraber.com>
2024-01-05 02:51:04 -0500

    Add the Immich logo as a vector (#4)

---
+++ icons/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+  id("com.eygraber.conventions-kotlin-multiplatform")
+  id("com.eygraber.conventions-android-library")
+  id("com.eygraber.conventions-detekt")
+  id("com.eygraber.conventions-compose-jetbrains")
+}
+
+android {
+  namespace = "app.immich.kmp.icons"
+}
+
+kotlin {
+  sourceSets {
+    commonMain {
+      dependencies {
+        implementation(compose.ui)
+      }
+    }
+  }
+}
